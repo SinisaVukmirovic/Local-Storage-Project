3f5f1e6885be92f846ea50c7538d89c35d109e17
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-05-04 20:18:55 +0200

    Creating Student Elem in DOM

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### Notes
 
+##### .append() method is used when we want to append multiple elems
+##### .appendChild() method is used when we want to append single elem
--- script.js
+++ script.js
@@ -5,15 +5,36 @@ const nameInput = studentForm['name'];
 const ageInput = studentForm['age'];
 const roleInput = studentForm['role'];
 
-const students = [];
+const students = [
+    {
+        name: 'John',
+        age: 32,
+        role: 'Mathematics'
+    }
+];
 
 const addStudent = (name, age, input) => {
 
 }
 
-const createStudent = () => {
+const createStudentElem = ({ name, age, role }) => {
+    // creating elements
     const studentDiv = document.createElement('div');
+
     const studentName = document.createElement('h3');
     const studentAge = document.createElement('p');
     const studentRole = document.createElement('p');
+
+    // filling the content
+    studentName.innerText = `Student name: ${name}`;
+    studentAge.innerText = `Student age: ${age}`;
+    studentRole.innerText = `Student role: ${role}`;
+
+    // adding to the DOM
+    studentDiv.append(studentName, studentAge, studentRole)
+    studentsContainer.appendChild(studentDiv)
 }
+
+students.forEach(student => {
+    createStudentElem(student);
+});
